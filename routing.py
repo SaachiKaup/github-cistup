46c053a250f25ba261713d397ecbf546554062c7
Saachi Kaup <saachi.kaup@gmail.com>
2024-12-16 08:53:34 +0530

    Removed locator module

--- routing.py
+++ routing.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import osmnx as ox
 import streamlit as st
-from locator import lat_lng_from_address
 
 PLACE = "Bangalore, India"
 
@@ -14,15 +13,6 @@ def add_lengths(G):
 
 G = graph(PLACE)
 G = add_lengths(G)
-
-gdf_nodes, gdf_edges = ox.convert.graph_to_gdfs(
-    G, nodes=True, edges=True,
-    node_geometry=True, fill_edge_geometry = True)
-
-projected_nodes = ox.projection.project_gdf(gdf_nodes)
-projected_edges = ox.projection.project_gdf(gdf_edges)
-
-G = ox.convert.graph_from_gdfs(projected_nodes, projected_edges)
 
 src_lat_lng = st.text_input("Enter Source Latitude, Longitude")
 dest_lat_lng = st.text_input("Enter Destination Latitude, Longitude")
@@ -42,19 +32,6 @@ if src_lat_lng and dest_lat_lng:
 
         print(src_node, src_dist, dest_node, dest_dist)
 
-        src_nearest_edges, src_edge_dist = ox.nearest_edges(G, from_lat, from_long, return_dist = True) 
-        dest_nearest_edges, dest_edge_dist = ox.nearest_edges(G, to_lat, to_long, return_dist = True) 
-        print(src_nearest_edges, dest_nearest_edges)
         routes = ox.k_shortest_paths(G, src_node, dest_node, k = 3, weight = "length")
         fig, ax = ox.plot_graph_routes(G, routes, route_color = "y", route_linewidth = 3)
         st.pyplot(fig)
-
-
-
-#orig_node = ox.nearest_nodes(G, X0, Y0)
-#dest_node = ox.nearest_nodes(G, X[0], Y[0])
-#
-#route = ox.shortest_path(G, orig_node, dest_node, weight="length")
-#fig, ax = ox.plot_graph_route(G, route, route_color="y", route_linewidth=6, node_size=0)
-#
-#st.pyplot(fig)
